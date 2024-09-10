561d66e99d9c481d4c6ee07036df8c805e6e8349
mayyyitn <tnn03754@gmail.com>
2024-09-10 22:29:26 +0700

    commit

---
+++ helloword/src/main/java/duytan/com/controllers/HomeController.java
@@ -0,0 +1,36 @@
+package duytan.com.controllers;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns= {"/home","/trangchu"})
+
+public class HomeController extends HttpServlet {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.setContentType("text/html");
+		String ten1 = req.getParameter("ten");
+		String ho1 = req.getParameter("ho");
+		PrintWriter printW = resp.getWriter();
+		printW.println(ho1 + " " +ten1);
+		
+		printW.close();
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+}
